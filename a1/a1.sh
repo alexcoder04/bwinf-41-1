266ba3eb7fc761baebbf1770099cc544646a7d15
alexcoder04 <alexcoder04@protonmail.com>
2022-09-28 22:58:11 +0200

    improve a1

--- a1/a1.sh
+++ a1/a1.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+# this script is posix compliant, so we can use /bin/sh
+
+TASK_FOLDER="${TASK_FOLDER:-./material/Aufgabe1}"
 
 echo "-------------------------------------------------"
-for file in $(ls "./material/Aufgabe1/stoerung"*".txt"); do
+for file in $(ls "$TASK_FOLDER/stoerung"*".txt"); do
     echo "Matches for '${file##*/}':"
-    cat "./material/Aufgabe1/Alice_im_Wunderland.txt" \
-        | tr "\\n" " " \
-        | tr '[:upper:]' '[:lower:]' \
-        | sed -e 's/[[:space:]]\+/ /g; s/[^(a-zäöüß )]//g;' \
+    sed \
+	-e '/\n/d; s/\(.*\)/\L\1/g; s/[[:space:]]\+/ /g; s/[^(a-zäöüß )]//g;' \
+	"$TASK_FOLDER/Alice_im_Wunderland.txt" \
         | grep -Eo "$(sed 's/_/\\S+/g' "$file")" \
         | uniq
     echo "-------------------------------------------------"
 done
+
